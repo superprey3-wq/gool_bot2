d258b8029a0307528a99eb106a58daaaee0fd4a3
superprey3-wq <super.prey3@gmail.com>
2026-09-06 12:20:05 +0300

    Install live brain fast lane

--- src/gool_bot2/__init__.py
+++ src/gool_bot2/__init__.py
@@ -2,6 +2,8 @@
 
 from .architecture_isolation import install_architecture_isolation
 from .production_calibration import install_production_calibration
+from .runtime_fastlane import install_runtime_fastlane
 
 install_architecture_isolation()
 install_production_calibration()
+install_runtime_fastlane()

3fb81f9cdbed36a9ac7725a8fcafa4d60a9497b1
superprey3-wq <super.prey3@gmail.com>
2026-09-06 11:34:51 +0300

    Install zero-signal production calibration

--- src/gool_bot2/__init__.py
+++ src/gool_bot2/__init__.py
@@ -1,5 +1,7 @@
 """gool_bot2 live-football probability engine."""
 
 from .architecture_isolation import install_architecture_isolation
+from .production_calibration import install_production_calibration
 
 install_architecture_isolation()
+install_production_calibration()
